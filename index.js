580e02ad6f482c68527a53f3147a038aabae4c7b
Arjun thakur <arjunthakur89096@gmail.com>
2023-03-29 00:13:31 +0530

    50%

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const mongoose=require("mongoose")
-let url='mongodb://127.0.0.1:27017/Inventory'
+const express=require('express')
+let app=express()
+let PORT=3000
+let dbConnection=require('./database/database')
+let mgConfig=require('./database/scema')
+
+app.use(express.json())
+app.post('/fetch',async (req,res)=>{
+   let query=''
+})
+app.post('/SignIn',async (req,res)=>{
     
-mongoose.connect(url)
+})
+
+app.listen(PORT,async()=>{
+    try {
+        await dbConnection()
+        console.log(`Port ${PORT}`)
+    }catch(error){
+        console.log(error)
+    }
+})
 
 /* 
-show dbs
+
 */
